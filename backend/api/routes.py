b9c0a61f670649dc8211ffd639cefcb36b9c460e
Andrew McGann <andrew.mcgann7@yahoo.com>
2025-04-13 20:25:02 -0700

    Addressing PR comments

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -1,5 +1,4 @@
-import json
-from flask import request
+from flask import request, jsonify
 from backend.api import app, cursor
 from dataclasses import asdict
 
@@ -8,139 +7,86 @@ from backend.models.department import Department
 from backend.models.ucore import Ucore
 from backend.models.professor import Professor
 
-
-@app.route("/")
-def root():
-    return "Flask Root!"
-
-
-@app.route("/hello")
-def hello():
-    with cursor() as cur:
-        cur.execute("select 'Hello, world!'")
-        res = cur.fetchone()[0]
-    return f"Hello world from Flask! And here's a message from Postgres: {res}"
-
-
-def convert_record(record):  # datetime objects are not json serializable
+"""
+Converts datetime properties to isoformat,
+datateime objects are not json serializable but isoformat is
+"""
+def convert_record(record):
     record_dict = asdict(record)
     record_dict["created_at"] = record.created_at.isoformat()
     record_dict["updated_at"] = record.updated_at.isoformat()
     return record_dict
 
 
-# Returns all courses
+"""
+Returns all courses as an array of Courses
+"""
 @app.route("/api/v1/courses", methods=["GET"])
 def get_courses():
     with cursor() as cur:
         cur.execute("select * from courses")
         rows = cur.fetchall()
         courses = [Course(*row) for row in rows]
-        return json.dumps([convert_record(course) for course in courses])
+        return jsonify([convert_record(course) for course in courses])
 
 
-# Returns all departments
+"""
+Returns all departments as an array of Departments
+"""
 @app.route("/api/v1/departments", methods=["GET"])
 def get_departments():
     with cursor() as cur:
         cur.execute("select * from departments")
         rows = cur.fetchall()
         departments = [Department(*row) for row in rows]
-        return json.dumps([convert_record(department) for department in departments])
+        return jsonify([convert_record(department) for department in departments])
 
 
-# Returns all ucores
+"""
+Returns all ucores as an array of Ucores
+"""
 @app.route("/api/v1/ucores", methods=["GET"])
 def get_ucores():
     with cursor() as cur:
         cur.execute("select * from ucores")
         rows = cur.fetchall()
         ucores = [Ucore(*row) for row in rows]
-        return json.dumps([convert_record(ucore) for ucore in ucores])
+        return jsonify([convert_record(ucore) for ucore in ucores])
 
 
-# Returns the top 3 professors for a specific subject and class level
-# /api/v1/top_professors?subject={department name}&class_level={level}
+"""
+Returns the top 3 professors for a given course as an array of Professors
+URL Request: /api/v1/top_professors?course_id={id}
+
+:param course_id: The id of the course to search for professors
+:return: A Professor array of the top 3 professors in json format
+"""
 @app.route("/api/v1/top_professors", methods=["GET"])
 def get_top_professors():
-    subject = request.args.get("subject")
-    class_level = request.args.get("class_level")
+    course_id = request.args.get("course_id")
 
-    if not subject or not class_level:
-        return (
-            json.dumps(
-                {"error": "Both 'subject' and 'class_level' parameters are required!"}
-            ),
-            400,
-        )
+    if not course_id:
+        return jsonify([]), 200
 
     with cursor() as cur:
-        # Get department ID for the given subject
-        cur.execute(
-            """
-            SELECT id FROM departments
-            WHERE name = %s
-        """,
-            (subject,),
-        )
-        department_row = cur.fetchone()
-
-        if not department_row:
-            return (
-                json.dumps({"error": "Department not found for the given subject."}),
-                404,
-            )
-        department_id = department_row[0]
-
-        # Get the courses associated with the department and class level
-        cur.execute(
-            """
-            SELECT id FROM courses
-            WHERE level = %s AND id IN (
-                SELECT course_id FROM course_departments WHERE department_id = %s
-            )
-        """,
-            (class_level, department_id),
-        )
-        course_rows = cur.fetchall()
-        course_ids = [row[0] for row in course_rows]
-
-        if not course_ids:
-            return (
-                json.dumps(
-                    {"error": "No courses found for the given subject and class level."}
-                ),
-                404,
-            )
-
-        # Create a SQL-safe list of placeholders for the IN clause
-        placeholders = ",".join(["%s"] * len(course_ids))
-
-        # Get top 3 professors based on their cumulative ratings for the courses
+        # Get top 3 professors based on their cumulative ratings for the course
         cur.execute(
             f"""
-            SELECT p.id, p.name, p.department_id, AVG(r.rating) as avg_rating
+            SELECT p.id, p.name, p.department_id, r.rating
             FROM professors p
             JOIN professor_cumulative_ratings r ON p.id = r.professor_id
             JOIN professor_course_ratings pcr ON p.id = pcr.professor_id
-            WHERE p.department_id = %s AND pcr.course_id IN ({placeholders})
-            GROUP BY p.id, p.name, p.department_id
-            ORDER BY avg_rating DESC
+            WHERE pcr.course_id = %s
+            GROUP BY p.id, p.name, p.department_id, r.rating
+            ORDER BY r.rating DESC
             LIMIT 3
-        """,
-            [department_id] + course_ids,
+            """,
+            (course_id,),
         )
 
         rows = cur.fetchall()
         if not rows:
-            return (
-                json.dumps(
-                    {
-                        "message": "No top professors found for the given subject and class level."
-                    }
-                ),
-                404,
-            )
+            return jsonify([]), 200
 
         # Create Professor objects from the result
         professors = [
@@ -153,24 +99,32 @@ def get_top_professors():
             )
             for row in rows
         ]
-        return json.dumps([asdict(professor) for professor in professors])
+        return jsonify([asdict(professor) for professor in professors]), 200
 
 
-# Returns the top 3 classes for a specific credit amount, class level, subject, and ucore (optional)
-# /api/v1/top_classes?credits={credit amount}&class_level={level}&subject={department name}&ucore={ucore id}
+"""
+Returns the top 3 classes for a given credit amount, class level, department, and ucore (optional).
+URL Request: /api/v1/top_classes?credits={amount}&class_level={level}&department_id={id}&ucore={id}
+
+:param credits: The credits of the courses to search for
+:param class_level: The class level of the courses to search for
+:param department_id: The id of the courses' department to search for
+:param ucore: Optional parameter to specify the ucore of the courses
+:return: A Course array of the top 3 courses in json format
+"""
 @app.route("/api/v1/top_classes", methods=["GET"])
 def get_top_classes():
     credits = request.args.get("credits")
     class_level = request.args.get("class_level")
-    subject = request.args.get("subject")
+    dept_id = request.args.get("department_id")
     ucore_id = request.args.get("ucore")  # optional
 
     # Validate required params
-    if not all([credits, class_level, subject]):
-        return {"error": "Missing required query parameters."}, 400
+    if not all([credits, class_level, dept_id]):
+        return jsonify([]), 200
 
     # Base query
-    query = """
+    query = f"""
         SELECT c.id, c.name, c.credits, c.level, AVG(pcr.rating) as avg_rating
         FROM courses c
         LEFT JOIN professor_course_ratings pcr ON c.id = pcr.course_id
@@ -178,34 +132,26 @@ def get_top_classes():
         LEFT JOIN departments d ON cd.department_id = d.id
         LEFT JOIN course_ucores cu ON c.id = cu.course_id
         WHERE c.credits = %s
-        AND c.level = %s
-        AND d.name = %s
-    """
-
-    # Add ucore if available
-    params = [credits, class_level, subject]
-
-    if ucore_id:
-        query += " AND cu.ucore_id = %s"
-        params.append(ucore_id)
-
-    query += """
+          AND c.level = %s
+          AND d.id = %s
+          {'AND cu.ucore_id = %s' if ucore_id else ''}
         GROUP BY c.id
         ORDER BY avg_rating DESC
         LIMIT 3
     """
+
+    # Add ucore if available
+    params = [credits, class_level, dept_id]
+
+    if ucore_id:
+        params.append(ucore_id)
 
     # Execute query and return Courses
     with cursor() as cur:
         cur.execute(query, params)
         rows = cur.fetchall()
         if not rows:
-            return (
-                json.dumps(
-                    {"message": "No top classes found for the given parameters."}
-                ),
-                404,
-            )
+            return (jsonify([]), 200)
 
         courses = [
             Course(
@@ -218,4 +164,4 @@ def get_top_classes():
             )
             for row in rows
         ]
-        return json.dumps([asdict(course) for course in courses])
+        return jsonify([asdict(course) for course in courses]), 200
